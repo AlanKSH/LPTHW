de739f890a7df3e55c036c6b03fa866f9b81d994
Yaocheng Wang <yocson@outlook.com>
2016-01-05 11:32:11 +0800

    Upload

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv#argument variable
+#from modules import
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first is variable is :", first
+print "Your second variable is:", second
+print "Your third variable is:", third
